f40bcffad3f842d6b5ae7e49e10a007161bb94a9
Florencia Lafuente <lafuente.florencia@gmail.com>
2019-09-13 17:59:49 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var  { schema } = require('./schema.js')
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
